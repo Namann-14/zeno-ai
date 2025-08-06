ac9b2a9b381bd0604471affca67b16d81677b1df
Namann-14 <namannayak.16@gmail.com>
2025-08-06 23:41:10 +0530

    feat: integrate Convex for backend functionality
    - Added Convex client to the widget app and set up user fetching.
    - Implemented ConvexProvider in the Providers component for state management.
    - Updated package.json to include Convex dependency.
    - Configured TypeScript paths for backend integration.
    - Created backend structure with Convex functions for user management.
    - Generated API and server utilities for Convex functions.
    - Defined schema for users in the backend.
    - Added example query and mutation functions for user data handling.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,18 @@
+'use client'
 import { Button } from "@workspace/ui/components/button"
+import { useQuery, useMutation } from  "convex/react"
+import { api } from "@workspace/backend/_generated/api"
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const newUser = useMutation(api.users.add);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        <Button onClick={() => newUser()}>Add user</Button>
+        {JSON.stringify(users)}
       </div>
     </div>
   )
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,11 +1,17 @@
+'use client'
 import { Button } from "@workspace/ui/components/button"
+import { useQuery } from "convex/react"
+import { api } from "@workspace/backend/_generated/api"
+
 
 export default function Page() {
+  const user = useQuery(api.users.getMany);
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        {JSON.stringify(user)}
       </div>
     </div>
   )
