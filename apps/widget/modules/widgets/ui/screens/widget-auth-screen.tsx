2430edb05bfc9b78426e8bdebe6f122853ccabf8
Namann-14 <namannayak.16@gmail.com>
2025-08-11 23:56:56 +0530

    feat: add WidgetAuthScreen component with form validation; implement create mutation for contact sessions

---
+++ apps/widget/modules/widgets/ui/screens/widget-auth-screen.tsx
@@ -0,0 +1,88 @@
+import z from "zod";
+import { useForm, ControllerRenderProps } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+} from "@workspace/ui/components/form";
+import { Button } from "@workspace/ui/components/button";
+import { Input } from "@workspace/ui/components/input";
+import { WidgetHeader } from "../components/widget-header";
+
+const formSchema = z.object({
+    name: z.string().min(1, "Name is required"),
+    email: z.string().email("Invalid email address"),
+});
+
+export const WidgetAuthScreen = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+    }
+
+    return (
+        <>
+            <WidgetHeader>
+                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
+                    <p className="text-3xl">
+                        Hi there
+                    </p>
+                    <p className="text-lg">
+                        Let&apos;s get  you started
+                    </p>
+                </div>
+            </WidgetHeader>
+            <Form {...form}>
+                <form
+                    className="flex flex-1 flex-col gap-y-4 p-4"
+                    onSubmit={form.handleSubmit(onSubmit)}
+                >
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }: { field: ControllerRenderProps<z.infer<typeof formSchema>, "name"> }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        className="h-10 bg-background"
+                                        placeholder="e.g. John Doe"
+                                        type="text"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }: { field: ControllerRenderProps<z.infer<typeof formSchema>, "email"> }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <Input
+                                        className="h-10 bg-background"
+                                        placeholder="e.g. johndoe@example.com"
+                                        type="email"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </form>
+            </Form>
+        </>
+    )
+}
--- apps/widget/modules/widgets/ui/views/widget-view.tsx
+++ apps/widget/modules/widgets/ui/views/widget-view.tsx
@@ -2,6 +2,7 @@
 
 import { WidgetFooter } from "../components/widget-footer";
 import { WidgetHeader } from "../components/widget-header";
+import { WidgetAuthScreen } from "../screens/widget-auth-screen";
 
 interface Props {
     organizationId: string;
@@ -10,18 +11,9 @@ interface Props {
 export const WidgetView = ({ organizationId }: Props) => {
     return (
         <main className="flex h-full w-full flex-col overflow-hidden rounded-xl border-bg-muted">
-            <WidgetHeader>
-                <div className="flex flex-col justify-between gap-y-2 px-2 py-6">
-                    <p className="text-3xl">
-                        Hi there
-                    </p>
-                    <p className="text-lg">
-                        How can we help you today?
-                    </p>
-                </div>
-            </WidgetHeader>
+            <WidgetAuthScreen />
             Widget View: {organizationId}
-            <WidgetFooter />
+            {/* <WidgetFooter /> */}
         </main>
     )
 }
